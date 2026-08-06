bc26a4dbddab8300ef98acd8ef822c67403f1fa3
arian <arian.maki@yahoo.com>
2026-08-07 00:59:32 +0200

    remove logo from sign-in & sign-up page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,8 @@
 import { ReactNode } from "react";
-import Logo from "../components/Logo";
 
 function layout({ children }: { children: ReactNode }) {
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center">
-      <Logo />
       <div className="mt-12">{children}</div>
     </div>
   );
