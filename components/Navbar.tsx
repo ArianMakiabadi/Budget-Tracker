ab3f87ea974f090f84f41fadd7bb1a2fcd70bb53
arian <arian.maki@yahoo.com>
2026-08-09 01:31:17 +0200

    feat(navbar): mobile navbar added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,20 @@
 "use client";
 import { usePathname } from "next/navigation";
-import Logo from "./Logo";
+import Logo, { LogoMobile } from "./Logo";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "./ui/button";
 import { UserButton } from "@clerk/nextjs";
 import ThemeSwitcherBtn from "./ThemeSwitcherBtn";
+import { useState } from "react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Menu } from "lucide-react";
 
 function Navbar() {
   return (
     <>
       <DesktopNavbar />
+      <MobileNavbar />
     </>
   );
 }
@@ -20,6 +24,44 @@ const items = [
   { title: "Transactions", link: "/transactions" },
   { title: "Manage", link: "/manage" },
 ];
+
+function MobileNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="block md:hidden border-separate bg-background">
+      <nav className="container flex items-center justify-between px-8">
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger
+            className={buttonVariants({ variant: "ghost", size: "icon" })}
+          >
+            <Menu />
+          </SheetTrigger>
+          <SheetContent className="w-100 sm:w-135" side="left">
+            <Logo />
+            <div className="flex flex-col pt-4 gap-1">
+              {items.map((item) => (
+                <NavbarItem
+                  key={item.title}
+                  title={item.title}
+                  link={item.link}
+                  onClick={() => setIsOpen(false)}
+                />
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+        <div className="flex h-20 min-h-15 items-center gap-x-4">
+          <LogoMobile />
+        </div>
+        <div className="flex items-center gap-2">
+          <ThemeSwitcherBtn />
+          <UserButton afterSwitchSessionUrl="/sign-in" />
+        </div>
+      </nav>
+    </div>
+  );
+}
 
 function DesktopNavbar() {
   return (
@@ -46,7 +88,15 @@ function DesktopNavbar() {
   );
 }
 
-function NavbarItem({ title, link }: { title: string; link: string }) {
+function NavbarItem({
+  title,
+  link,
+  onClick,
+}: {
+  title: string;
+  link: string;
+  onClick?: () => void;
+}) {
   const pathName = usePathname();
   const isActive = pathName === link;
 
@@ -59,6 +109,9 @@ function NavbarItem({ title, link }: { title: string; link: string }) {
           "w-full justify-start text-lg text-muted-foreground hover:text-foreground",
           isActive && "text-foreground",
         )}
+        onClick={() => {
+          if (onClick) onClick();
+        }}
       >
         {title}
       </Link>
