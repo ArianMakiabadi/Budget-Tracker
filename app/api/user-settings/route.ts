c4404df5c2d01c0de4cc6060883995d3f829021c
arian <arian.maki@yahoo.com>
2026-08-24 19:22:45 +0200

    fix: replace PostgreSQL adapter with SQLite adapter

--- app/api/user-settings/route.ts
+++ app/api/user-settings/route.ts
@@ -3,19 +3,19 @@ import { currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-export async function GET(request: Request) {
+export async function GET() {
   const user = await currentUser();
 
   if (!user) redirect("/sign-in");
 
-  let userSettings = prisma.userSettings.findUnique({
+  let userSettings = await prisma.userSettings.findUnique({
     where: {
       userId: user.id,
     },
   });
 
   if (!userSettings) {
-    userSettings = prisma.userSettings.create({
+    userSettings = await prisma.userSettings.create({
       data: {
         userId: user.id,
         currency: "USD" /*default currency*/,
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,22 @@
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "./generated/prisma/client";
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+
+const adapter = new PrismaBetterSqlite3({
+  url: "file:./dev.db",
 });
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
 const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
     adapter,
   });
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
 export default prisma;
